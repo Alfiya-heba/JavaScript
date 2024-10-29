4f30893499d37ceed88d70949af32210a10516a3
Alfiya Heba <157894259+Alfiya-heba@users.noreply.github.com>
2024-10-29 14:44:28 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"Use Strict" //Treat all JS code as newer version
+
+// alert(3+3) // We are using nodejs, not browser
+
+// console.log(3+3)
+// console.log("Alfiya")
+  
+
+// Data Types//
+
+// let name="Alfiya" //String
+// let age=22 //number
+// let isLoggedIn= false //Boolean
+// let fname=null //null (Standalone)
+// let a; //undefined 
+
+
+//typeOf//
+
+// console.log(typeof "Alfiya") //To identify the type typeOf
+// console.log(typeof age);
+// console.log(typeof null); // null type will be object//
+console.log(typeof undefined); // undefined type is undefined//
