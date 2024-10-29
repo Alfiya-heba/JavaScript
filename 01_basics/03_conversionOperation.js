2d4577bad917d7c65a1aabb4cfd66b0a315b5855
Alfiya Heba <157894259+Alfiya-heba@users.noreply.github.com>
2024-10-29 15:13:44 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score="10abc";
+// 2 Methods to find the type//
+// console.log(typeof score);
+// console.log(typeof (score));// using function Eg:(score)
+
+//String to Number//
+
+ let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // Nan= Not a Number// //Output//
+
+
+
+ let isLoggedIn=1
+ let booleanisLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn) 
+
+// 1 -- True
+// 0 --false
+// "" --false
+// "Alfiya"--true
+
+
+//Number to string//
+let someNumber=16
+let stringNumber=String(someNumber)
+console.log( typeof stringNumber);
