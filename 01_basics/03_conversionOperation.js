ef07dd3ed324ed8ebaf4c35b50f096ee6c9f5fa0
Alfiya Heba <157894259+Alfiya-heba@users.noreply.github.com>
2024-10-29 16:09:54 +0000

    Operations of dataTpes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,6 @@
+//*****************Conversions***************** */
+
+
 let score="10abc";
 // 2 Methods to find the type//
 // console.log(typeof score);
@@ -24,4 +27,25 @@ let score="10abc";
 //Number to string//
 let someNumber=16
 let stringNumber=String(someNumber)
-console.log( typeof stringNumber);
+// console.log( typeof stringNumber);
+
+
+//*****************Operations************************//
+
+let value=10
+let negValue=-value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); //Power value//
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1="Hello"
+let str2=" Alfiya"
+let str3= str1+str2
+console.log(str3);
+
