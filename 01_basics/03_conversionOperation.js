5ebe49b30816cf42f0bc449792bfeeb8a5da89bf
Alfiya Heba <157894259+Alfiya-heba@users.noreply.github.com>
2024-10-29 18:12:48 +0000

    Operator dataTypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -47,5 +47,32 @@ let negValue=-value
 let str1="Hello"
 let str2=" Alfiya"
 let str3= str1+str2
-console.log(str3);
+// console.log(str3);
+
+
+// console.log(1+1);//2
+// console.log("1"+1);//11
+// console.log(1+"1");//11
+// console.log("1"+2+2);//122
+// console.log(1+2+"2");//32
+// console.log((3+4)*5%3);//2
+
+// console.log(+true);//1
+// console.log(+" ");//0
+
+// let gameCounter=100
+// ++gameCounter;
+// console.log(gameCounter);
+
+// //Postfix//
+// let x=3;
+// let y=x++;
+// console.log(y);
+// // x is 4 and y is 3//
+
+//prefix//
+let x=4;
+let y=++x;
+console.log(y);
+// x is 5 and y is 5//
 
