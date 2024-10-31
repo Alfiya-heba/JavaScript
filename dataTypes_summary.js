e94d3963f83e503d7c9621540c1bdbb356cad978
Alfiya Heba <157894259+Alfiya-heba@users.noreply.github.com>
2024-10-31 18:53:52 +0000

    dataType summary

---
+++ dataTypes_summary.js
@@ -0,0 +1,38 @@
+//  Primitive Data Type 7 Types:
+//  String
+//  Number
+//  Boolean
+//  null(empty)
+//  undefined
+//  Symbol (make a value unique)
+//  BigInt
+
+
+//Symbol idea//
+const id=Symbol("alfiya");
+const anotherId=Symbol("alfiya");
+console.log(id==anotherId);//false
+
+// Reference Type or Non Primitive Data Type:
+// Arrays
+// Objects
+// Functions
+
+
+//Arrays//
+const names=["Alfiya","Heba","Nonu","Chintu"]
+
+//Objects//
+let myObj= {
+    name:"Alfiya",
+    age:21,
+}
+
+
+//Functions//
+
+function() {
+
+}
+
+// JavaScript is dynamically typed because it checks the type during the runtime.
