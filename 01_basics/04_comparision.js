b84b2dbe9a5a0c2d8eaafa0b66065d799c984670
Alfiya Heba <157894259+Alfiya-heba@users.noreply.github.com>
2024-10-31 07:01:08 +0000

    comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,19 @@
+// console.log(2>1);//true
+// console.log(2>=1);//true
+
+// console.log("2">1);
+
+// console.log(null>0); // false
+// console.log(null==0); //false
+// console.log(null>=0);//true because comparisons convert null to a number, treating null as 0
+
+// console.log(undefined>0);//false
+// console.log(undefined==0);//false
+// console.log(undefined>0);//false
+
+
+// ===(Strict check) //
+console.log(("2"===2)); //false because dataTypes are different 
+
+
+
