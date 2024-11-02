ef198a381dd1f6c8d76c7c5ea34d1a081ce13d71
Alfiya Heba <157894259+Alfiya-heba@users.noreply.github.com>
2024-11-02 19:01:17 +0000

    dates and time in JS

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,37 @@
+//***********Numbers************/
+
+const age=50
+//console.log(age);//50
+
+const newAge=new Number(50)
+//console.log(newAge); //[Number:50]
+
+//console.log(newAge.toString().length); //2
+//console.log(newAge.toFixed(1));
+
+const anotherAge=25.6778
+//console.log(anotherAge.toPrecision(3));
+
+const number=1000000
+//console.log(number.toLocaleString());
+
+
+//*********Maths**********//
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.9));
+// console.log(Math.floor(4.6)); // floor is the lowest value//
+// console.log(Math.ceil(4.1));// Ceil is the upper value or the heightest value//
+// console.log(Math.sqrt(25));
+// console.log(Math.pow(2,3));
+// console.log(Math.min(5,3,7,2));//2
+// console.log(Math.max(4,8,69));//69
+
+console.log(Math.random());
+console.log(Math.floor((Math.random()*6)+1));
+
+
+
+
+
---
+++ 01_basics/07_datesinJS.js
@@ -0,0 +1,37 @@
+//Dates//
+
+let myDate=new Date()
+
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+
+//console.log(typeof myDate);
+
+// let newDate=new Date(2024,9,16)
+// console.log(newDate.toDateString());// for specific date
+
+let newDate=new Date(2024,9,16,1,30,34)
+//console.log(newDate.toLocaleString());
+
+let myTimeStamp=Date.now()
+// console.log(myTimeStamp);
+// console.log(newDate.getTime());
+// console.log(Date.now()/1000);
+// console.log(Math.floor(Date.now()/1000));
+
+let mynewDate=new Date()
+console.log(mynewDate);
+console.log(mynewDate.getMonth()+1);// as it starts from 0 so it gives -1 month so add +1 it gives correct month
+console.log(mynewDate.getTime());
+
+
+
+
+
+
