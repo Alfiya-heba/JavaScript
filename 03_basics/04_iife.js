a5feb7776434924daf05e97838204a473b22ba29
Alfiya Heba <157894259+Alfiya-heba@users.noreply.github.com>
2024-11-05 13:05:41 +0000

    iife in js

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,13 @@
+(function alfi(){
+    console.log("Database Connected"); 
+}
+)();//semicolon is mandatory coz to end it 
+
+(function heba(){
+    console.log("Hi, Alfiya")
+}) ();
+
+//Arrow function//
+((name)=>{
+    console.log(`Hi,${name}`);
+})('Heba')
