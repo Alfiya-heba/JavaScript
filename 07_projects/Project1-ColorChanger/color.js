ffc2f9e2daad9272b47419547a52fbe28d05ca8a
Alfiya Heba <157894259+Alfiya-heba@users.noreply.github.com>
2024-11-14 14:19:00 +0000

    Project

---
+++ 07_projects/Project1-ColorChanger/color.js
@@ -0,0 +1,18 @@
+const buttons=document.querySelectorAll('.button')
+const body=document.querySelector("body")
+
+buttons.forEach(function(buttons){
+    console.log(buttons);
+    buttons.addEventListener('click',function(e){
+        console.log(e);
+        console.log(e.target);
+        if(e.target.id==='red'){
+            body.style.backgroundColor=red;
+        }
+        
+        
+
+    })
+    
+    
+})
